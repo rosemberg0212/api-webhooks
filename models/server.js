0f76f64e34db2b68ec5001a662e2570be9970d46
sebastian <rockmadero@gmail.com>
2023-11-21 14:11:58 -0500

    valio verga

--- models/server.js
+++ models/server.js
@@ -1,51 +1,48 @@
-const express = require('express')
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 class Server {
-    constructor(){
-        this.app = express()
-        this.port = process.env.PORT;
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
 
-        this.paths = {
-            certificado:   '/hook/certificado',
-            horario:       '/hook/horario',
-            pqrdRomi:      '/hook/pqrs'
-        }
+    this.paths = {
+      certificado: "/hook/certificado",
+      horario: "/hook/horario",
+    };
 
-        //Middlewares
-        this.Middlewares()
+    //Middlewares
+    this.Middlewares();
 
-        //Rutas de mi aplicacion
-        this.routes()
-    }
+    //Rutas de mi aplicacion
+    this.routes();
+  }
 
-    async conectarDB(){
-        await dbConnection()
-    }
+  async conectarDB() {
+    await dbConnection();
+  }
 
-    Middlewares(){
-        //CORS
-        this.app.use(cors());
+  Middlewares() {
+    //CORS
+    this.app.use(cors());
 
-        //lectura y parseo del body
-        this.app.use(express.json());
+    //lectura y parseo del body
+    this.app.use(express.json());
 
-        //directorio publico
-        this.app.use(express.static('public'))
+    //directorio publico
+    this.app.use(express.static("public"));
+  }
 
-    }
-    
-    routes(){
-        this.app.use(this.paths.certificado, require('../routes/certificado'));
-        this.app.use(this.paths.horario, require('../routes/horarios'));
-        this.app.use(this.paths.pqrdRomi, require('../routes/pqrsRomi'));
-    }
+  routes() {
+    this.app.use(this.paths.certificado, require("../routes/certificado"));
+    this.app.use(this.paths.horario, require("../routes/horarios"));
+  }
 
-    listem(){
-        this.app.listen(this.port, ()=>{
-            console.log('corriendo en el puerto', this.port)
-        }) 
-    }
+  listem() {
+    this.app.listen(this.port, () => {
+      console.log("corriendo en el puerto", this.port);
+    });
+  }
 }
 
 module.exports = Server;
