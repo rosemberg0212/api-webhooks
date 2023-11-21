7e39e3a673848d9dab2759dfbed19374fe7c5031
sebastian <rockmadero@gmail.com>
2023-11-21 08:32:07 -0500

    pqrs romi route endpoint

--- controllers/pqrsRomiC.js
+++ controllers/pqrsRomiC.js
@@ -1,8 +1,8 @@
 const { enviarHorarioWhatsApp } = require("../helpers/apiBotmaker");
 const enviarFormPQRS = async (req, res) => {
   const challenge = req.body.challenge;
+  res.send({ challenge });
 
-  // const id = req.body.event.pulseId;
   const id = req.body.event.pulseId;
 
   const query = `query { boards(ids: 5469236502) { id items (ids: ${id}) { id name column_values { id title text } } } }`;
@@ -10,8 +10,7 @@ const enviarFormPQRS = async (req, res) => {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization:
-        "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjI3MzE5NzY4MSwiYWFpIjoxMSwidWlkIjo0NjQ5MzM3MiwiaWFkIjoiMjAyMy0wOC0wNFQyMDo0MzoyMC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTM2OTY2OTMsInJnbiI6InVzZTEifQ._UBoJp-3wNdz5Wj_ITu-QyGG-uTrcf3nXcCrK7rugG4",
+      Authorization: process.env.APIKEY_MONDAY,
     },
     body: JSON.stringify({
       query: query,
@@ -24,11 +23,6 @@ const enviarFormPQRS = async (req, res) => {
       const categoria = data.boards[0].items[0].column_values[2].text;
       const anotacion = data.boards[0].items[0].column_values[3].text;
       const telefono = data.boards[0].items[0].column_values[4].text;
-
-      // ! De pruebas borrar al terminar.
-      // res
-      //   .status(200)
-      //   .json({ challenge, statusPeticion, categoria, anotacion, telefono });
 
       // ?  Romi
       await enviarHorarioWhatsApp(
