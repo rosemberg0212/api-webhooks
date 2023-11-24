d03d175a1bf7443d2d8a735c8f43c08177941aad
sebastian <rockmadero@gmail.com>
2023-11-24 09:14:23 -0500

    console json

--- controllers/taskRyverC.js
+++ controllers/taskRyverC.js
@@ -1,9 +1,9 @@
 const infoRyver = async (req, res) => {
   const challenge = req.body.challenge;
   res.send({ challenge });
-  const {data} = req.body;
+  const data = req.body;
   try {
-    console.log(data.channel.__metadata);
+    console.log(JSON.stringify(data));
   } catch (error) {
     console.log(error);
   }
