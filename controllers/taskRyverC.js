35c525fbadacdac9d15f0bd067d847d42037ab9c
sebastian <rockmadero@gmail.com>
2023-11-24 10:26:43 -0500

    prueba 1 de mensaje task

--- controllers/taskRyverC.js
+++ controllers/taskRyverC.js
@@ -1,9 +1,18 @@
+const { enviarHorarioWhatsApp } = require("../helpers/apiBotmaker");
+
 const infoRyver = async (req, res) => {
   const challenge = req.body.challenge;
   res.send({ challenge });
-  const data = req.body;
+  const { data } = req.body;
+  const desc = data.entity.subject;
+  const category = data.entity.category.descriptor;
+  const telefono = desc.split(" - ")[1];
+  const servicio = desc.split(" - ")[0];
   try {
-    console.log(JSON.stringify(data));
+    await enviarHorarioWhatsApp(
+      telefono,
+      `Hola, tu servicio de ${servicio} ha pasado a ${category}`
+    );
   } catch (error) {
     console.log(error);
   }
