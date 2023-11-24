3af1b5ad9fcd331168de9df9e6efbab22abb9672
rous <rous@gmail.com>
2023-11-24 14:27:21 -0500

    nomina avance

--- controllers/nomina.js
+++ controllers/nomina.js
@@ -7,9 +7,9 @@ const calcularNomina = async (req, res) => {
     const apikey = process.env.APIKEY_MONDAY;
 
     // const id = req.body.event.pulseId;
-    const id = '5482696148';
+    const id = '5567378603';
 
-    const query = `query { boards(ids: 5482696120) { id items (ids: ${id}) { id name column_values { id title text } } } }`;
+    const query = `query { boards(ids: 5474798239) { id items (ids: ${id}) { id name column_values { id title text } } } }`;
     const response = await fetch("https://api.monday.com/v2", {
         method: 'POST',
         headers: {
@@ -28,13 +28,24 @@ const calcularNomina = async (req, res) => {
             const datosMonday = data
             // console.log(datosMonday.data.boards[0].items[0].column_values)
             const datosT = datosMonday.data.boards[0].items[0].column_values;
+            // console.log(datosMonday.data.boards[0].items[0].name)
+            const nombre = datosMonday.data.boards[0].items[0].name
 
-            console.log(datosT)
-
-            const filtro = datosT.filter(dato=>{
-                return dato.id =! 'estado'
+            const filtro = datosT.filter(dato => {
+                return dato.id !== 'estado' && dato.id !== 'tel_fono' && dato.id !== 'cargo0' && dato.id !== 'bot_n'
             })
             console.log(filtro)
+
+            let contador = 0
+
+            filtro.map(dato => {
+                if (dato.text == 'RS1' || dato.text == 'Novedad 1') {
+                    contador++
+                }
+            })
+
+            await mandarNomina(contador, nombre)
+            console.log(contador)
         } else {
             console.error('Hubo un error en la solicitud.');
             console.error('Código de estado:', response.status);
@@ -45,6 +56,35 @@ const calcularNomina = async (req, res) => {
         console.error('Hubo un error en la solicitud:', error);
     }
     res.status(200).end();
+}
+
+const mandarNomina = async (dias, nombre) => {
+
+    const query = `mutation {create_item (board_id: 5567451793, group_id: \"topics\", item_name: \"${nombre}\", column_values: \"{\\\"n_meros6\\\":\\\"${dias}\\\"}\") {id}}`
+    const response = await fetch("https://api.monday.com/v2", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': process.env.APIKEY_MONDAY
+        },
+        body: JSON.stringify({
+            'query': query
+        })
+    });
+
+    try {
+        if (response.ok) {
+            const data = await response.json();
+            console.log(JSON.stringify(data))
+        } else {
+            console.error('Hubo un error en la solicitud.');
+            console.error('Código de estado:', response.status);
+            const errorMessage = await response.text();
+            console.error('Respuesta:', errorMessage);
+        }
+    } catch (error) {
+        console.error('Hubo un error en la solicitud:', error);
+    }
 }
 
 module.exports = {
