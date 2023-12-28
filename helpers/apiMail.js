145db315748ae631b29e9d850833f77248119a7f
rous <rous@gmail.com>
2023-12-28 14:24:17 -0500

    correo rh

--- helpers/apiMail.js
+++ helpers/apiMail.js
@@ -1,17 +1,18 @@
+const nodemailer = require("nodemailer");
+
 const probandoMail = async (cuerpo, mail) => {
-    const nodemailer = require("nodemailer");
 
     const config = {
         host: "smtp.gmail.com",
         port: 587,
         auth: {
-            user: "rosemberc.g.02@gmail.com",
+            user: "gestionhumana@gehsuites.com",
             pass: process.env.MAIL_CONTRA_APP,
         },
     };
 
     const mensaje = {
-        from: "rosemberc.g.02@gmail.com",
+        from: "gestionhumana@gehsuites.com",
         to: `${mail}`,
         subject: "Notificacion Horario Laboral",
         text: `${cuerpo}`,
