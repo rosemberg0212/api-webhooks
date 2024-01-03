9d9eae64ec5aeba4873ef6a2a397bf8ce0f0f805
rous <rous@gmail.com>
2024-01-03 11:25:50 -0500

    una sola funcion

--- routes/horarios.js
+++ routes/horarios.js
@@ -2,33 +2,11 @@ const {Router} = require('express');
 
 const { 
     enviarHorarios,
-    enviarHorariosReservas, 
-    enviarHorariosMantenimiento, 
-    enviarHorariosSantaM, 
-    enviarHorariosRodadero, 
-    enviarHorariosAvexi,
-    enviarHorariosAzuan,
-    enviarHorariosAbi,
-    enviarHorariosBocagrande,
-    enviarHorariosWindsor,
-    enviarHorariosMadisson,
-    enviarHorariosMarina
 } = require('../controllers/horariosC');
 
 const router = Router();
 
 router.post('/', enviarHorarios);
-router.post('/reservas', enviarHorariosReservas);
-router.post('/mantenimiento', enviarHorariosMantenimiento);
-router.post('/santaM', enviarHorariosSantaM);
-router.post('/rodadero', enviarHorariosRodadero); 
-router.post('/avexi', enviarHorariosAvexi); 
-router.post('/azuan', enviarHorariosAzuan); 
-router.post('/abi', enviarHorariosAbi); 
-router.post('/bocag', enviarHorariosBocagrande);  
-router.post('/windsor', enviarHorariosWindsor);  
-router.post('/madisson', enviarHorariosMadisson);  
-router.post('/marina', enviarHorariosMarina);  
 
 
 module.exports = router
