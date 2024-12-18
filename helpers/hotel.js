c9a5a3a44a55dc484446351e89ef8bbb609cf8dd
rous <rous@gmail.com>
2024-12-18 08:13:54 -0500

    enel

--- helpers/hotel.js
+++ helpers/hotel.js
@@ -157,8 +157,8 @@ const empresaRecibo = (empresa) => {
                 empresa": "nombre de la empresa".
                 "tipo_servicio": "tipo de servicio".
                 "numero_contrato": "numero de contrato o NIC o POLIZA".
-                "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo, en formato año/mes/dia.
-                "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando)".
+                "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo, en formato año/mes/dia (el año no puede ser menor al año en el que estamos actualmente).
+                "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando, el año no puede ser menor al año en el que estamos actualmente)".
                 valor_consumo: el valor total que se encuentra en la seccion de ACUEDUCTO Y ALCANTARILLADO, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0
                 valor_servicios: el valor total que se encuentra en la seccion de ASEO, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0
                 valor_total_a_pagar: el valor total a pagar, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0`
@@ -169,8 +169,8 @@ const empresaRecibo = (empresa) => {
                 empresa": "nombre de la empresa".
                 "tipo_servicio": "tipo de servicio".
                 "numero_contrato": "numero de contrato o NIC o POLIZA".
-                "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia.
-                "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando)".
+                "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia (el año no puede ser menor al año en el que estamos actualmente).
+                "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando, el año no puede ser menor al año en el que estamos actualmente)".
                 valor_consumo: el valor total que se encuentra en la primera seccion de Energia, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0
                 valor_servicios: el valor total que se encuentra en la seccion de alumbrado publico, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0
                 valor_total_a_pagar: el valor total a pagar, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0`
@@ -181,8 +181,8 @@ const empresaRecibo = (empresa) => {
                 empresa": "nombre de la empresa".
                 "tipo_servicio": "tipo de servicio".
                 "numero_contrato": "es el campo llamado Id interno".
-                "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia.
-                "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando)".
+                "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia (el año no puede ser menor al año en el que estamos actualmente).
+                "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando, el año no puede ser menor al año en el que estamos actualmente)".
                 valor_consumo: el valor total que se encuentra en la seccion Detalles cobros de energia en el item de Energia + contribución, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0
                 valor_servicios: el valor total que se encuentra en la seccion Detalles otros cobros en el item de Alumbrado Pub Vlr, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0
                 valor_total_a_pagar: el valor total a pagar, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0`
@@ -193,8 +193,8 @@ const empresaRecibo = (empresa) => {
                 empresa": "nombre de la empresa".
                 "tipo_servicio": "tipo de servicio".
                 "numero_contrato": "numero de contrato o NIC o POLIZA".
-                "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia.
-                "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando)".
+                "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia (el año no puede ser menor al año en el que estamos actualmente).
+                "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando, el año no puede ser menor al año en el que estamos actualmente)".
                 valor_consumo: el valor total a pagar, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0
                 valor_servicios: colocalo en 0
                 valor_total_a_pagar: el valor total a pagar, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0`
@@ -205,8 +205,8 @@ const empresaRecibo = (empresa) => {
                 empresa": "nombre de la empresa".
                 "tipo_servicio": "tipo de servicio".
                 "numero_contrato": "numero de contrato o NIC o POLIZA".
-                "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia.
-                "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando)".
+                "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia (el año no puede ser menor al año en el que estamos actualmente).
+                "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando, el año no puede ser menor al año en el que estamos actualmente)".
                 valor_consumo: es el valor que se encuentra en la seccion de Energía que suma los item de Energía Mes + Estado de cuenta, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0.
                 valor_servicios: es el valor a pagar que resulta de la suma de las secciones Otros Servicios + Impuestos y Tasas, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0.
                 valor_total_a_pagar: el valor total a pagar, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0`
@@ -217,8 +217,8 @@ const empresaRecibo = (empresa) => {
                 empresa": "nombre de la empresa".
                 "tipo_servicio": "tipo de servicio".
                 "numero_contrato": "numero de contrato o NIC o POLIZA".
-                "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia.
-                "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando)".
+                "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia (el año no puede ser menor al año en el que estamos actualmente).
+                "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando, el año no puede ser menor al año en el que estamos actualmente)".
                 valor_consumo: el valor total a pagar, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0
                 valor_servicios: colocalo en 0
                 valor_total_a_pagar: el valor total a pagar, (coloca solo los numeros, no pongas los puntos o comas), si no hay colocar 0`
@@ -229,8 +229,8 @@ const empresaRecibo = (empresa) => {
             empresa": "nombre de la empresa".
             "tipo_servicio": "tipo de servicio".
             "numero_contrato": "numero de contrato o NIC o POLIZA".
-            "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia.
-            "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando)".
+            "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia (el año no puede ser menor al año en el que estamos actualmente).
+            "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando, el año no puede ser menor al año en el que estamos actualmente)".
             valor_consumo: es el valor total que se encuentra en el item de Acueducto (coloca solo los numeros, y no tengas en cuenta lo que esta despues de un punto porque son decimales, solo toma encuenta lo que esta antes y despues de una coma)
             valor_servicios: es el valor total que se encuentra en el item de Alcantarillado, (coloca solo los numeros, y no tengas en cuenta lo que esta despues de un punto porque son decimales, solo toma encuenta lo que esta antes y despues de una coma), 
             valor_total_a_pagar: el valor total a pagar, (coloca solo los numeros, y no tengas en cuenta lo que esta despues de un punto porque son decimales, solo toma encuenta lo que esta antes y despues de una coma),`
@@ -241,8 +241,8 @@ const empresaRecibo = (empresa) => {
             empresa": "nombre de la empresa".
             "tipo_servicio": "tipo de servicio".
             "numero_contrato": "numero de contrato o NIC o POLIZA".
-            "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia.
-            "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando)".
+            "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia (el año no puede ser menor al año en el que estamos actualmente).
+            "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando, el año no puede ser menor al año en el que estamos actualmente)".
             valor_consumo: el valor total a pagar
             valor_servicios: 0
             valor_total_a_pagar: el valor total a pagar`
@@ -253,8 +253,8 @@ const empresaRecibo = (empresa) => {
             empresa": "nombre de la empresa".
             "tipo_servicio": "tipo de servicio".
             "numero_contrato": "numero de contrato o NIC o POLIZA".
-            "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia.
-            "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando)".
+            "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia (el año no puede ser menor al año en el que estamos actualmente).
+            "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando, el año no puede ser menor al año en el que estamos actualmente)".
             valor_consumo: el valor total a pagar
             iva: valor total del iva
             valor_servicios: 0
@@ -266,8 +266,8 @@ const empresaRecibo = (empresa) => {
             empresa": "nombre de la empresa".
             "tipo_servicio": "tipo de servicio".
             "numero_contrato": "numero de contrato o NIC o POLIZA".
-            "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia.
-            "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando)".
+            "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia (el año no puede ser menor al año en el que estamos actualmente).
+            "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando, el año no puede ser menor al año en el que estamos actualmente)".
             valor_consumo: el valor total a pagar
             iva: valor total del iva
             valor_servicios: 0
@@ -279,8 +279,8 @@ const empresaRecibo = (empresa) => {
             empresa": "nombre de la empresa".
             "tipo_servicio": "tipo de servicio".
             "numero_contrato": "numero de contrato o NIC o POLIZA".
-            "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia.
-            "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando)".
+            "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia (el año no puede ser menor al año en el que estamos actualmente).
+            "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando, el año no puede ser menor al año en el que estamos actualmente)".
             valor_consumo: el valor total a pagar
             iva: valor total del iva
             valor_servicios: 0
@@ -292,11 +292,24 @@ const empresaRecibo = (empresa) => {
             "empresa": "nombre de la empresa".
             "tipo_servicio": "tipo de servicio".
             "numero_contrato": "numero de contrato o NIC o POLIZA".
-            "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia.
-            "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando)".
+            "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia (el año no puede ser menor al año en el que estamos actualmente).
+            "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando, el año no puede ser menor al año en el que estamos actualmente)".
             valor_consumo: el valor total a pagar
             iva: valor total del iva
             valor_servicios: 0
+            valor_total_a_pagar: el valor total a pagar`
+            return prom;
+
+        case 'Enel':
+            prom = `Te voy a mandar una imagen sobre un recibo público de la empresa enel, necesito que analices y me extraigas la siguiente información en formato json:
+            "empresa": "nombre de la empresa".
+            "tipo_servicio": "tipo de servicio".
+            "numero_contrato": "numero de cliente o numero de cuenta".
+            "fecha_limite_pago": puede ser también fecha de corte o ultimo día de pago sin recargo o fecha de pago oportuno, en formato año/mes/dia (el año no puede ser menor al año en el que estamos actualmente).
+            "periodo_consumo": "año/mes/dia (si no hay dia colocar el primer dia del mes que se esta cobrando, el año no puede ser menor al año en el que estamos actualmente)".
+            valor_consumo: es el total energia
+            iva: valor total del iva
+            valor_servicios: es el total aseo
             valor_total_a_pagar: el valor total a pagar`
             return prom;
         default:
